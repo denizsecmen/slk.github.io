c14146976077c55358a1cd4119649cd6e2e604f4
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-08-20 21:26:31 +0300

    Mobile version of layout is finished

--- script/script.js
+++ script/script.js
@@ -2,3 +2,14 @@ var myCarousel = document.querySelector('#carouselExampleControls');
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 5000 
 });
+let Status = false;
+$("#vnb_button").on("click", function () {
+  if (Status) {
+    $("#vnavbar").slideUp();
+    Status = false;
+  }
+  else {
+    $("#vnavbar").slideDown();
+    Status = true;
+  }
+})

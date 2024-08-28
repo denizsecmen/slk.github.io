c432209fff5e15d5d42ade49a2ae053af8086ed6
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-08-28 21:42:12 +0300

    Perfomance test of vertical navbar 0

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
 var myCarousel = document.querySelector('#carouselExampleControls');
+let scrollvnavbar = document.querySelector('#vnavbar');
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 5000 
 });
 let Status = false;
+$("#layout  .dropdown").on('mouseout', function(e){
+  $(this).children('ul').css('display', 'none');
+}
+);
+$("#layout  .dropdown").on('mouseover', function(e){
+  $(this).children('ul').css('display', 'block');
+}
+);
+$("#selection_list > .dropdown button").on('click', function(e){
+  console.log("Button Clicked");
+  try {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  catch (err) {
+    console.log(err);
+  }
+});
 $("#vnb_button").on("click", async function (e) {
   if (Status) {
     $("#vnavbar").slideUp();
